5d0c753534efc7421dab7f8ea445c201ceaf68e4
mfzszgs <su_wy@outlook.com>
2014-12-30 18:40:22 +0800

    a

---
+++ matrix_mul/build/CMakeFiles/MatrixMul.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/MatrixMul.dir/MatrixMul.cpp.o"
+  "MatrixMul.pdb"
+  "MatrixMul"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/MatrixMul.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
